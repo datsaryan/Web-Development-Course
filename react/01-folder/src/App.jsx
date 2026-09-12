08e47c243a5b03f11014d261b37d7491b9f0caa7
datsaryan <aryansobdh@gmail.com>
2026-09-12 09:35:02 +0530

    tic tac toe game

---
+++ react/01-folder/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return <>
+  <div className="dad"></div>
+  <h1>My App</h1>
+  <h1>My App1</h1>
+  <div className="chacha"></div>
+  </>
+}
+
+export default App
